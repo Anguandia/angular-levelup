9185fda8f1b91f2faa505074fb83d80f130c7984
Anguandia <mikeanguandia@gmail.com>
2020-03-04 21:00:24 +0200

    add ci

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,14 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    flags: [
+      '--disable-web-security',
+      '--disable-gpu',
+      '--no-sandbox'
+    ]
   });
 };
